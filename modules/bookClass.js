f2494c6749568fe8941b177c32432d5572e67437
principles31 <principles31@gmail.com>
2022-05-11 11:04:27 +0000

    Update modules file

--- modules/bookClass.js
+++ modules/bookClass.js
@@ -4,30 +4,30 @@ export default class BookCL {
     this.author = author;
   }
 
-  books = JSON.parse(window.localStorage.getItem("bookData") || "[]");
+  books = JSON.parse(window.localStorage.getItem('bookData') || '[]');
 
   removeBook(title) {
-    this.books = JSON.parse(window.localStorage.getItem("bookData"));
+    this.books = JSON.parse(window.localStorage.getItem('bookData'));
     const removebk = this.books.filter((book) => book.title !== title);
-    window.localStorage.setItem("bookData", JSON.stringify(removebk));
+    window.localStorage.setItem('bookData', JSON.stringify(removebk));
   }
 }
 
 // eslint-disable-next-line class-methods-use-this
 export const displayBook = (booksCl) => {
-  booksCl.books = JSON.parse(window.localStorage.getItem("bookData") || "[]");
+  booksCl.books = JSON.parse(window.localStorage.getItem('bookData') || '[]');
 
   /** Loop thgrough book object and create inner html based on the data inside the
    * book object
    */
   // eslint-disable-next-line no-restricted-syntax
   for (const book of [...Object.keys(booksCl.books)]) {
-    const bookContainer = document.querySelector(".book-container");
-    const element = document.createElement("div");
-    const button = document.createElement("button");
-    button.innerHTML = "Remove";
-    button.classList.add("remove");
-    element.classList.add("book-list");
+    const bookContainer = document.querySelector('.book-container');
+    const element = document.createElement('div');
+    const button = document.createElement('button');
+    button.innerHTML = 'Remove';
+    button.classList.add('remove');
+    element.classList.add('book-list');
     element.innerHTML = `
         <div class='p-list'>
               <p class="titleBook">${booksCl.books[book].title}</p>
@@ -40,7 +40,7 @@ export const displayBook = (booksCl) => {
 
     /** Add function click event to the explicitly declared button */
     // eslint-disable-next-line no-loop-func
-    button.addEventListener("click", (e) => {
+    button.addEventListener('click', (e) => {
       const targetClass = e.target.parentElement;
       const stringTitle = targetClass.childNodes[1].childNodes[1].textContent;
       booksCl.removeBook(stringTitle);
